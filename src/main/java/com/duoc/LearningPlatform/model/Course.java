f0769854eb24a573209791edb4d1f22a1608ce16
ctlnaelena <cata.portilla@duocuc.cl>
2026-04-13 10:42:34 -0400

    Update Course.java

--- src/main/java/com/duoc/LearningPlatform/model/Course.java
+++ src/main/java/com/duoc/LearningPlatform/model/Course.java
@@ -5,11 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Course {
-    @Id private int id;
+    @Id 
+    private int id;
     private String name;
     private String category;
 
